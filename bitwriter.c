fe2274722dcbe5d88979582f451570529b7fe32b
takizawa <nihey.takizawa@gmail.com>
2014-03-27 00:39:51 -0300

    Add bitwritting manipulating functionality.
    It was added some bit-by-bit I/O manipulation, primarily to be used on writting
    bits into a file.

---
+++ bitwriter.c
@@ -0,0 +1,59 @@
+#include "bitwriter.h"
+
+int BitWriter_init(BitWriter *writer, char *filename) {
+    writer->buffer = 0;
+    writer->bit_number = 0;
+
+    writer->file = fopen(filename, "w");
+
+    return (int) writer->file;
+}
+
+int BitWriter_write_bit(BitWriter *writer, unsigned char bit) {
+    // Prepare the bit so it has one set signal (won't corrupt anything)
+    bit &= 1;
+    // Shift buffer one position
+    writer->buffer <<= 1;
+    // Set the value on the proper position
+    writer->buffer |= bit;
+
+    writer->bit_number++;
+    // In case of buffer being full, we flush everything to the file
+    if(writer->bit_number == 8) {
+        return BitWriter_flush(writer);
+    }
+
+    return 0;
+}
+
+int BitWriter_write_bits(BitWriter *writer, unsigned char bitstream, int count) {
+    int i;
+    int retval = 0;
+    for(i = 0; i < count; i++) {
+        // Get the left most bit and walks to the right untill the end
+        unsigned char bit = (bitstream & (1 << (count - i - 1)))?1:0;
+        retval |= BitWriter_write_bit(writer, bit);
+    }
+    return retval;
+}
+
+int BitWriter_flush(BitWriter *writer) {
+    // If there are no contents, there's nothing to write
+    if(writer->bit_number == 0) {
+        return 0;
+    }
+
+    writer->buffer <<= 8 - writer->bit_number;
+
+    int retval = fwrite(&(writer->buffer), sizeof(char), 1, writer->file);
+    writer->buffer = 0;
+    writer->bit_number = 0;
+
+    return !retval;
+}
+
+int BitWriter_close(BitWriter *writer) {
+    int flush = BitWriter_flush(writer);
+    int close = fclose(writer->file);
+    return flush || close;
+}
---
+++ bitwriter.h
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef BITWRITER
+
+typedef struct BitWriter {
+
+    // Bits to be written in the FILE
+    unsigned char buffer;
+    // Number of bit that where currently written in the buffer
+    int bit_number;
+    // File pointer for writting
+    FILE *file;
+
+} BitWriter;
+
+/**
+    Initializes a bitwriter, all bitwriter files are opened in "wb" mode.
+ */
+int BitWriter_init(BitWriter *writer, char *filename);
+
+/**
+    Write a bit to the bitwriter, if the bitwriter buffer is full it will
+    flush it to the file (flushing does not mean calling fflush to force the
+    writing).
+ */
+int BitWriter_write_bit(BitWriter *writer, unsigned char bit);
+
+/**
+    Writes a stream of bits into the file.
+
+    @param count The number of bit to extract from the bitstream
+*/
+int BitWriter_write_bits(BitWriter *writer, unsigned char bitstream, int count);
+
+/**
+    Force writing every content on the buffer to the file
+ */
+int BitWriter_flush(BitWriter *writer);
+
+/**
+    Flushes the buffer and closes the writer
+ */
+int BitWriter_close(BitWriter *writer);
+
+#endif // BITWRITER
