9c72e3443a301236fa42a93acceb90f8046b8d51
Bruno Orlandi <brorlandi@gmail.com>
2014-05-10 15:44:35 -0300

    Samples files, run.bat for Windows, minor fix in main.

--- main.c
+++ main.c
@@ -4,44 +4,68 @@ This algorithm was implemented by Bruno Orlandi. 2014
 
 #include <stdio.h>
 #include "huffman.h"
+#include <string.h>
+#include "fileio.h"
 
 int main(int argc, char *argv[]){
-    return main_test(argc,argv);
-/*
-    if(argc < 2){ // the file must be in the arguments
-        printf("Usage: [input file] , if the input file ends with '.hff' it will be decompressed otherwise will be compressed in a '.hff' file.\n");
+    //return main_test(argc,argv)
+
+    if(argc < 3){ // the file must be in the arguments
+        printf("Usage: <input file> [optional -d] <output file>\n\t -d\tThe input file will be decompressed to output file.");
         return 0;
     }
+    char *inputFile = argv[1];
+    char *outputFile;
 
-    char *fileName = argv[1]; // name of the file to be compressed or decompressed
+    char decompress = 0;
+    if(!strcmp(argv[2],"-d")){
+        decompress = 1;
+        if(argc < 4){
+            printf("Missing <output file>\n");
+            return 0;
+        }
+        outputFile = argv[3];
+    }
+    else{
+        outputFile = argv[2];
+    }
 
-    if(endsWith(fileName,".hff")){ // check if the file is an '.hff' file, if it is, decompress
+    Huffman h;
+
+    Huffman_init(&h);
+
+    if(decompress){
+
+        printf("Decompressing: %s\n", inputFile);
 
         unsigned int size; // size of the file to be calculated
         unsigned char *data; // new data after decompress
 
-        huffmanFileRead(fileName,&data,&size);
+        Huffman_file_decompress(&h,inputFile);
+        data = h.uc_data[0];
+        size = h.uc_sizes[0];
 
         // here can make others manipulations to the data
 
-        printf("Saving the uncompressed file.\n");
-        int len = strlen(fileName);
-        fileName[len-4] = '\0';
-        fileWrite(fileName,data,size);
-        printf("File decompressed: %s\n",fileName);
+        printf("Saving the decompressed file.\n");
+        fileWrite(outputFile,data,size);
+        printf("File decompressed to: %s\n",outputFile);
     }
-    else{ // otherwise, the file will be compres8sed in a hff file
-
+    else
+    {
         unsigned int size; // size of the file to be calculated
         unsigned char *data; // the data read from the file
 
-        data = fileRead(fileName,&size); // read the file and stores in data
+        data = fileRead(inputFile,&size); // read the file and stores in data
 
+        printf("Compressing: %s\n", inputFile);
+        Huffman_add_data_block(&h,data,size);
 
-        huffmanFileWrite(fileName,data,size);
+        Huffman_apply(&h);
 
+        Huffman_compress_data_to_file(&h,outputFile);
+        printf("File compressed to: %s\n",outputFile);
     }
-
-*/
+    Huffman_free(&h);
     return 0;
 }
--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
-.\bin\Debug\Huffman.exe rio.bmp
-.\bin\Debug\Huffman.exe rio.bmp.hff
+.\bin\Debug\Huffman.exe "sample.bmp" "sample.bmp.hff"
+.\bin\Debug\Huffman.exe "sample.bmp.hff" -d "decompressed sample.bmp"
+
+.\bin\Debug\Huffman.exe "sample.txt" "sample.txt.hff"
+.\bin\Debug\Huffman.exe "sample.txt.hff" -d "decompressed sample.txt"
+pause
